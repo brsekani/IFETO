e5bafc5088659200325a61c0e821ccd0b39dcec2
brsekani <lawaltemidayo06@gmail.com>
2025-11-29 14:52:46 +0100

    first commit

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,69 +1,10 @@
-"use client";
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import Image from "next/image";
-import Logo from "@/assets/images/IFETO-Logo-1.png";
-import authImage from "@/assets/images/loginBG.png";
-import authImage2 from "@/assets/images/authImg2.png";
-import AuthSliderMobile from "@/components/auth/AuthSliderMobile";
-import AuthSliderDesktop from "@/components/auth/AuthSliderDesktop";
-import LoginForm from "@/components/auth/LoginForm";
-import useLogin from "@/hooks/form-hooks/useLogin";
-import useRedirectIfAuthenticated from "@/hooks/useRedirectIfAuthenticated";
+import LoginClient from "@/components/auth/LoginClient";
+import { Suspense } from "react";
 
-const page = () => {
-  const { loading } = useRedirectIfAuthenticated();
-
-  const { formik, isLogining } = useLogin();
-  const [backgroundIndex, setBackgroundIndex] = useState(0);
-
-  const backgroundImages = [authImage, authImage2];
-
-  if (loading) {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        <p className="text-gray-500">Loading...</p>
-      </div>
-    );
-  }
-
+export default function Page() {
   return (
-    <section className="w-full min-h-screen flex flex-col lg:flex-row overflow-hidden lg:bg-[#F9F9F9] bg-white">
-      {/* left side */}
-      <div className="w-full lg:w-1/2 lg:px-[45px] lg:py-20 mt-3 mb-10">
-        {/* logo */}
-        <div className="flex justify-center items-center">
-          <Image src={Logo} alt="logo" className="lg:w-[266px] w-[152px]" />
-        </div>
-
-        {/* mobile slider */}
-        <AuthSliderMobile />
-
-        {/* forms */}
-        <LoginForm formik={formik} isLogining={isLogining} />
-      </div>
-
-      {/* right side desktop*/}
-      <div className="relative hidden lg:block w-full lg:w-1/2 overflow-hidden">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={backgroundIndex}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.6 }}
-            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-            style={{
-              backgroundImage: `url(${backgroundImages[backgroundIndex].src})`,
-            }}
-          />
-        </AnimatePresence>
-        <div className="absolute inset-0 bg-[#00000033]"></div>
-        <div className="absolute bottom-14 left-10 right-10 z-10">
-          <AuthSliderDesktop onIndexChange={setBackgroundIndex} />
-        </div>
-      </div>
-    </section>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginClient />
+    </Suspense>
   );
-};
-export default page;
+}
